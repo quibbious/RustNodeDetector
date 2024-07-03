58489fef66d15183089a466557801af262e4c467
quibb <108188557+quibbious@users.noreply.github.com>
2024-07-03 15:23:21 -0500

    Update db.py
    "security features"

--- db.py
+++ db.py
@@ -1,7 +1,10 @@
-# download dataset from roboflow: 
-
 from roboflow import Roboflow
-rf = Roboflow(api_key="YOUR_API_KEY")
+# download dataset from roboflow: 
+API_KEY="YOUR_API_KEY" # replace "YOUR_API_KEY" with your own api key
+if API_KEY == "YOUR_API_KEY":
+  raise Exception("Hey! it looks like you forgot to input your API key! Be sure to save db.py, and never share your API_KEY")
+  
+rf = Roboflow(api_key=API_KEY)
 project = rf.workspace("theoldtofu").project("rustnodedetector")
 version = project.version(6)
 dataset = version.download("yolov5")
